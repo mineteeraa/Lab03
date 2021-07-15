cbf8fc9717fe323a062480e3f691e56589330b45
mineteeraa <patteera_mine@hotmail.com>
2021-07-15 21:46:13 +0700

    [ADD] create EventCard, Categories, and add data in home

---
+++ src/components/Categories.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="category-card">
+    <h5>@{{ category.category }} on {{ category.organizer }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Categories',
+  props: {
+    category: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.category-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  text-align: right;
+}
+
+.category-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
